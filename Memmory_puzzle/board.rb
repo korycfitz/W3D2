14cfe4ac1d831130d57b1b51031f13f811980796
pko912 <kopeter912@gmail.com>
2022-09-27 14:26:01 -0400

    hi

--- Memmory_puzzle/board.rb
+++ Memmory_puzzle/board.rb
@@ -1,9 +1,23 @@
 class Board
-    def initialize
-        @grid = Array.new(size) { Array.new(size)}
 
+    # def self.populate(length) #
+    #     raise 'length must be even' if length.odd? #rescue and retry.
+    #     arr = [1,2,3,4,5,6,7,8,9,10].sample(length / 2)
+    #     new_arr = arr.dup 
+    #     total_cards = arr.shift(new_arr)
+    # end
 
+    def initialize(num)
+        raise 'length must be even' if length.odd?
+        @grid = Array.new(num) { Array.new(num)}
+        @size = num * num
         
     end
 
-end
+
+
+
+
+
+
+end 
